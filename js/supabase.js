7b566cd7122310db134837961ab09b22bbcc7047
harshitbhatia07 <168970403+harshitbhatia07@users.noreply.github.com>
2026-06-03 20:37:58 +0000

    upload

---
+++ js/supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://nqatwpkjzzkksnnefbyk.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_yJ4ycOOr8L-4rBGohXZwWQ_ZRc4_tEW";
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
