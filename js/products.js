935397bf3d5e26f24bdef8f6f51ef5f50d2442c8
harshitbhatia07 <168970403+harshitbhatia07@users.noreply.github.com>
2026-06-07 06:47:46 +0000

    upload

---
+++ js/products.js
@@ -0,0 +1,38 @@
+const products = [
+
+{
+id:1,
+name:"White Luxury Sneaker",
+price:650,
+image:"assets/products/white-luxury.jpg"
+},
+
+{
+id:2,
+name:"Black Luxury Sneaker",
+price:650,
+image:"assets/products/black-luxury.jpg"
+},
+
+{
+id:3,
+name:"White Air Runner",
+price:1450,
+image:"assets/products/white-air-1.jpg"
+},
+
+{
+id:4,
+name:"White Air Runner Premium",
+price:1450,
+image:"assets/products/white-air-2.jpg"
+},
+
+{
+id:5,
+name:"Black Air Runner",
+price:1450,
+image:"assets/products/black-air.jpg"
+}
+
+]; 
